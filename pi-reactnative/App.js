3fbefa9ea2df2f29d761d8d2370f139e40cc1fda
catascallan <cscallan@udesa.edu.ar>
2024-11-11 20:34:28 -0300

    Empiezo Profile

--- pi-reactnative/App.js
+++ pi-reactnative/App.js
@@ -1,17 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RegisterScreen from './src/screens/RegisterScreen';
+import LoginScreen from './src/screens/LoginScreen'; 
+import HomeMenu from './src/components/HomeMenu'; 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Register">  
         <Stack.Screen 
           name="Register" 
           component={RegisterScreen} 
-          options={{headerShown: false}} 
+          options={{ headerShown: false }}  
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen}  
+          options={{ headerShown: false }}  
+        />
+        <Stack.Screen 
+          name="HomeMenu" 
+          component={HomeMenu}  
+          options={{ headerShown: false }}  
         />
       </Stack.Navigator>
     </NavigationContainer>
